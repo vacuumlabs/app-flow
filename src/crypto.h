0d1aceb1d5360d368ed8559496435681a8e23ece
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2024-01-03 14:29:58 +0100

    Signing refactor to introduce message signing

--- src/crypto.h
+++ src/crypto.h
@@ -46,6 +46,7 @@ zxerr_t crypto_sign(const hd_path_t path,
                     const uint16_t options,
                     const uint8_t *message,
                     uint16_t messageLen,
+                    const uint8_t domainTag[DOMAIN_TAG_LENGTH],
                     uint8_t *signature,
                     uint16_t signatureMaxlen,
                     uint16_t *sigSize);
