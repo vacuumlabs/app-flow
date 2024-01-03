db8cd3b184d13efe831175b06c49d230fd281a01
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2024-01-03 21:59:02 +0100

    Clang-format

--- src/common/tx.c
+++ src/common/tx.c
@@ -52,12 +52,10 @@ const uint8_t TX_DOMAIN_TAG[DOMAIN_TAG_LENGTH] = {
 };
 
 void tx_initialize() {
-    buffering_init(
-            ram_buffer,
-            sizeof(ram_buffer),
-            (uint8_t *) N_appdata.buffer,
-            sizeof(N_appdata.buffer)
-    );
+    buffering_init(ram_buffer,
+                   sizeof(ram_buffer),
+                   (uint8_t *) N_appdata.buffer,
+                   sizeof(N_appdata.buffer));
 }
 
 void tx_reset() {
--- src/tx_metadata.h
+++ src/tx_metadata.h
@@ -1,18 +1,18 @@
 /*******************************************************************************
-*   (c) 2022 Vacuumlabs
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-********************************************************************************/
+ *   (c) 2022 Vacuumlabs
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ********************************************************************************/
 
 #pragma once
 
@@ -32,19 +32,22 @@ typedef enum {
     ARGUMENT_TYPE_OPTIONALARRAY = 4
 } argument_type_e;
 
-#define PARSER_MAX_ARGCOUNT 9
-#define METADATA_HASH_SIZE 32 //CX_SHA256_SIZE
+#define PARSER_MAX_ARGCOUNT          9
+#define METADATA_HASH_SIZE           32  // CX_SHA256_SIZE
 #define MAX_METADATA_MAX_ARRAY_ITEMS 20
 
-//It is planned that all these strings may be on flash, thus they are volatile (for NanoX and NanoSPlus)
+// It is planned that all these strings may be on flash, thus they are volatile (for NanoX and
+// NanoSPlus)
 typedef struct {
     argument_type_e argumentType;
-    uint8_t arrayMinElements; //defined only for ARGUMENT_TYPE_ARRAY and ARGUMENT_TYPE_OPTIONALARRAY
-    uint8_t arrayMaxElements; //defined only for ARGUMENT_TYPE_ARRAY and ARGUMENT_TYPE_OPTIONALARRAY
-    const char *displayKey; 
+    uint8_t
+        arrayMinElements;  // defined only for ARGUMENT_TYPE_ARRAY and ARGUMENT_TYPE_OPTIONALARRAY
+    uint8_t
+        arrayMaxElements;  // defined only for ARGUMENT_TYPE_ARRAY and ARGUMENT_TYPE_OPTIONALARRAY
+    const char *displayKey;
     uint8_t displayKeyLength;
-    uint8_t argumentIndex; //argument index within transaction
-    const char *jsonExpectedType; //pointer to null terminated string
+    uint8_t argumentIndex;         // argument index within transaction
+    const char *jsonExpectedType;  // pointer to null terminated string
     uint8_t jsonExpectedTypeLength;
     jsmntype_t jsonExpectedKind;
 } parsed_tx_metadata_argument_t;
@@ -53,23 +56,28 @@ typedef struct {
     const char *txName;
     uint8_t txNameLength;
     uint8_t argCount;
-    parsed_tx_metadata_argument_t arguments[PARSER_MAX_ARGCOUNT]; //order of arguments in which they should be displayed
+    parsed_tx_metadata_argument_t
+        arguments[PARSER_MAX_ARGCOUNT];  // order of arguments in which they should be displayed
 } parsed_tx_metadata_t;
-
 
 void initStoredTxMetadata();
 
 parser_error_t storeTxMetadata(const uint8_t *txMetadata, uint16_t txMetadataLength);
 
-parser_error_t validateStoredTxMetadataMerkleTreeLevel(const  uint8_t* hashes, size_t hashesLen);
+parser_error_t validateStoredTxMetadataMerkleTreeLevel(const uint8_t *hashes, size_t hashesLen);
 
-parser_error_t parseTxMetadata(const uint8_t scriptHash[METADATA_HASH_SIZE], parsed_tx_metadata_t *parsedTxMetadata);
+parser_error_t parseTxMetadata(const uint8_t scriptHash[METADATA_HASH_SIZE],
+                               parsed_tx_metadata_t *parsedTxMetadata);
 
 #ifdef TEST
-//For unit tests
-parser_error_t _parseTxMetadata(const uint8_t scriptHash[METADATA_HASH_SIZE], const uint8_t *metadata, size_t metadataLen, 
+// For unit tests
+parser_error_t _parseTxMetadata(const uint8_t scriptHash[METADATA_HASH_SIZE],
+                                const uint8_t *metadata,
+                                size_t metadataLen,
                                 parsed_tx_metadata_t *parsedTxMetadata);
-parser_error_t _validateScriptHash(const uint8_t scriptHash[METADATA_HASH_SIZE], const uint8_t *txMetadata, uint16_t txMetadataLength);
+parser_error_t _validateScriptHash(const uint8_t scriptHash[METADATA_HASH_SIZE],
+                                   const uint8_t *txMetadata,
+                                   uint16_t txMetadataLength);
 #endif
 
 #ifdef __cplusplus
